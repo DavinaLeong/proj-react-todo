7cca5b14995ed3649e90ca0fb2bda2f28f2db957
Personal Profile <davina.leong.sy@gmail.com>
2020-03-13 12:12:45 +0800

    Able to update title and body

--- app/src/pages/AddPage.js
+++ app/src/pages/AddPage.js
@@ -2,12 +2,54 @@ import React from 'react';
 
 import PageHeader from './../components/PageHeader';
 
-function AddPage(props) {
-  return (
-    <div>
-      <PageHeader title='Add' />
-    </div>
-  );
+class AddPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      body: '',
+      userId: null
+    };
+  }
+
+  updateTitle = (event) => {
+    this.setState({
+      title: event.target.value
+    });
+  }
+
+  updateBody = (event) => {
+    this.setState({
+      body: event.target.value
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <PageHeader title='Add' />
+
+        <form onSubmit={() => this.handleSubmit} method="POST">
+          <div>
+            <label>Title</label>
+            <input type="text" value={this.state.title} required onChange={this.updateTitle}/>
+          </div>
+
+          <div>
+            <label>Body</label>
+            <textarea rows="4" value={this.state.body} required onChange={this.updateBody}></textarea>
+          </div>
+
+          <input type="submit" value="Submit"/>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default AddPage;
