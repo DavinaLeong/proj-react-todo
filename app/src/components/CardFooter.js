6d2b56aa1379a7d1cac9e9eac61b2f68911921c2
Personal Profile <davina.leong.sy@gmail.com>
2020-03-13 10:16:59 +0800

    Render the post's creator's name

--- app/src/components/CardFooter.js
+++ app/src/components/CardFooter.js
@@ -1,11 +1,54 @@
 import React from 'react';
 
-function CardFooter(props) {
-  return (
-    <div className='card-footer'>
-      {props.text}
-    </div>
-  );
+class CardFooter extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      error: null,
+      isLoaded: false,
+      user: null
+    };
+  }
+
+  componentDidMount() {
+    // console.log(this.props);
+    fetch('https://jsonplaceholder.typicode.com/users/' + this.props.text)
+      .then(response => response.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            user: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+  render() {
+    let result = null;
+    const { error, isLoaded, user } = this.state;
+
+    if (error) {
+      result = <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
+      result = <div>Loading...</div>
+    } else {
+      result = user.name;
+    }
+
+    return (
+      <div className='card-footer'>
+        {result}
+      </div>
+    );
+  }
 }
 
 export default CardFooter;
