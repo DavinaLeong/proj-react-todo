6246b50c8016c01bde5be85d350d5aab85a74f48
Personal Profile <davina.leong.sy@gmail.com>
2020-03-13 17:13:31 +0800

    Fit all contents into a container

--- app/src/App.js
+++ app/src/App.js
@@ -36,7 +36,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="container">
         <Title />
         <Nav
           links={this.props.links}
