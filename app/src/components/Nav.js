b4282418b0c9f35004585a3c487f9008b4579b90
Personal Profile <davina.leong.sy@gmail.com>
2020-03-13 11:49:12 +0800

    Removed console log

--- app/src/components/Nav.js
+++ app/src/components/Nav.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 function Nav(props) {
   const items = [];
-  console.log(props);
   props.links.forEach((link, index) => {
     items.push(
       <li key={'l'+index}><button onClick={() => props.setPage(link.page)} type="button">{link.name}</button></li>
